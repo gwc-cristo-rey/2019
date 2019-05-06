4b6be5922d916da4fd5835012f47540b897f4850
Heidi Hernandez <hhernandez19@cristoreyatlanta.org>
2019-05-06 17:10:56 -0400

    Updated Quotes on Angry Page

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-var quotes = ["new value!", "Value 2$#@!Ff5h", "Value 3", "Value 4"];
-
+var quotes = ["When angry, count to four. When very angry, swear.\n-Mark Twain", "Those who are at war with others are not at peace with themselves.\n-William Hazlitt", "Smile. Breathe. And go slowly.\n-Thich Nhat Hanh", "Reactions come from the mind. Responses come from the heart.\n-Joseph Meyering Sr"];
 
 $(document).ready(function() {
-    var random = Math.floor(Math.random() * quotes.length);
-    var element = $("#random")[0];
-    element.innerText = quotes[random];
+    var randomOne = Math.floor(Math.random() * quotes.length);
+    var randomTwo = Math.floor(Math.random() * quotes.length);
+    var randomThree = Math.floor(Math.random() * quotes.length);
+    var elements = $(".random");
+    var firstQuote = elements[0];
+    var secondQuote = elements[1];
+    var thirdQuote = elements[2];
+    firstQuote.innerText = quotes[randomOne];
+    secondQuote.innerText = quotes[randomTwo];
+    thirdQuote.innerText = quotes[randomThree];
 }) 
